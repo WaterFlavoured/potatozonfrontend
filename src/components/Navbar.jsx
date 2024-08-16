67f9f03899ebc8baa93511f18d7e0dda1e52a860
WaterFlavoured <michaelz9478@gmail.com>
2024-08-16 15:00:39 -0400

    Setup the basics of Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '/image.png'
+function Navbar() {
+  return (
+    <nav>
+      <div className='logoAndImageCom'>
+        <img src={logo} alt="" />
+        <h1 className='logotitle'>POTATOZON</h1>
+      </div>
+      <form className='searchbar'>
+        <input type="text" className='bar' placeholder='Search Potatozon.ca'/>
+        <button type='submit' className='subButton'>Search</button>
+      </form>
+      <ul>
+        <li>Cart</li>
+        <li>Account</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
